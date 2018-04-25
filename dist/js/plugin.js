26c5b69679be57403b3e869753505eb42ce30658
Vladislav Khramov <khramovw@gmail.com>
2018-04-25 13:48:32 +0300

    animate elements

--- dist/js/plugin.js
+++ dist/js/plugin.js
@@ -4,10 +4,13 @@ var _createClass = function () { function defineProperties(target, props) { for 
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var tl = TweenLite;
+var twl = TweenLite;
+var tl = new TimelineLite();
 var slider = document.getElementById('slider-gaps');
 var paginationBtn = document.querySelectorAll('#pagination li a');
 var slideAll = slider.querySelectorAll('.slide');
+
+tl.staggerFrom(document.querySelectorAll('.btn-dot'), 0.15, { x: 0, y: -100, autoAlpha: 0, ease: Power4.easeOut, delay: 1 }, 0.1);
 
 var SliderGsap = function () {
     function SliderGsap() {
@@ -18,6 +21,7 @@ var SliderGsap = function () {
         this.activeSlide = 1;
         this.canGo = true;
         this.maxSlide = 5;
+        this.startSlide = 0;
     }
 
     _createClass(SliderGsap, [{
@@ -27,22 +31,25 @@ var SliderGsap = function () {
 
             slider.addEventListener('wheel', function (e) {
 
+                // console.log(e);
+
                 if (!_this.canGo) return;
                 _this.canGo = false;
 
                 var direction = e.wheelDeltaY < 0 ? 1 : -1;
-                var newSlide = +_this.activeSlide + +direction;
+                var newSlide = _this.activeSlide + direction;
 
                 if (newSlide > _this.maxSlide) newSlide = 1;
                 if (newSlide < 1) newSlide = 5;
 
-                PubSub.publish('gotoSlide', { from: _this.activeSlide, to: +newSlide });
+                PubSub.publish('gotoSlide', { from: _this.activeSlide, to: newSlide });
+                PubSub.publish('gotoSlide.scroll', { start: true });
 
                 _this.activeSlide = newSlide;
 
                 setTimeout(function () {
                     _this.canGo = true;
-                }, 600);
+                }, 1300);
             }, { passive: true });
         }
     }, {
@@ -62,7 +69,8 @@ var SliderGsap = function () {
 
                     if (newSlide !== _this2.activeSlide) {
 
-                        PubSub.publish('gotoSlide', { from: +_this2.activeSlide, to: +newSlide });
+                        PubSub.publish('gotoSlide', { from: _this2.activeSlide, to: newSlide });
+                        PubSub.publish('gotoSlide.top', { start: true, from: _this2.activeSlide, to: newSlide });
 
                         _this2.activeSlide = newSlide;
                     }
@@ -78,10 +86,89 @@ var slide = new SliderGsap();
 
 PubSub.subscribe('gotoSlide', function (msg, data) {
 
+    // console.log( msg, data );
+
+
+    //Show current slide
+    slideAll.forEach(function (sl, d, i) {
+
+        // console.log('-= sl =-',d, sl,'i|::',i);
+
+        if (sl.dataset.slide == data.from) {
+
+            sl.classList.remove('is-active');
+            sl.dataset.state = 'hidden';
+
+            if (data.from == 1) {
+                console.log('| data.from == 1 |', sl.childNodes);
+                tl.to(sl.childNodes[3].childNodes[1], 0.3, { autoAlpha: 0 }).to(sl.childNodes[3].childNodes[3], 0.3, { autoAlpha: 1 }, '-=0.3').to(sl, 0.25, { autoAlpha: 0, delay: 0.25 });
+            }
+            if (data.from == 2) {
+                console.log('| data.from == 2 |');
+                tl.to(sl.childNodes[3].childNodes[3], 0.3, { top: "16.2%", left: "23.8%", width: "42%", ease: Power4.easeOut }).to(sl.childNodes[3].childNodes[3], 0.1, { autoAlpha: 0, delay: 0.25 }).to(sl, 0.25, { autoAlpha: 0, ease: Power4.easeOut }, '-=0.3');
+            }
+            if (data.from == 3) {
+                console.log('| data.from == 3 |');
+
+                tl.to(sl.childNodes[3].childNodes[3], 0.3, { top: "44%", left: "32%", width: "41%", ease: Power4.easeOut }).to(sl.childNodes[3].childNodes[3], 0.1, { autoAlpha: 0, delay: 0.25 }).to(sl, 0, { autoAlpha: 0 }, '-=0.3');
+            }
+
+            console.log('*** sl.dataset.slide ***', sl.dataset.slide);
+
+            // tl.to(sl.childNodes[3].childNodes[3], 0.3, { top: "14.2%",left: "23.8%",width: "42%", delay: 0.5 })
+            //     .to(sl.childNodes[3].childNodes[3], 0.1, { autoAlpha:0})
+            //            .to(sl, 0.25, {autoAlpha:0, ease: Power4.easeOut}, '-=0.3');
+
+
+            sl.classList.add('is-hidden');
+
+            // console.log('-====',  sl.childNodes[3].childNodes[3])
+
+        }
+        if (sl.dataset.slide == data.to) {
+
+            sl.classList.add('is-active');
+            sl.dataset.state = 'visibility';
+
+            if (data.to == 1) {
+                console.log('| data.from == 1 |', sl.childNodes);
+
+                tl.to(sl, 0.25, { autoAlpha: 1, ease: Power4.easeOut }, '-=0.3').to(sl.childNodes[3].childNodes[3], 0.3, { autoAlpha: 0 }).to(sl.childNodes[3].childNodes[1], 0.3, { autoAlpha: 1 }, '-=0.3');
+            }
+
+            if (data.to == 2) {
+
+                tl.to(sl, 0, { autoAlpha: 1, ease: Power4.easeOut }, '-=0.3').to(sl.childNodes[3].childNodes[3], 0.1, { autoAlpha: 1 }, "-=0.5").to(sl.childNodes[3].childNodes[3], 0.3, { top: "47%", left: "-11%", width: "70%", ease: Power4.easeOut });
+            }
+
+            if (data.to == 3) {
+
+                tl.to(sl, 0, { autoAlpha: 1, ease: Power4.easeOut }, '-=0.3').to(sl.childNodes[3].childNodes[3], 0.1, { autoAlpha: 1 }, "-=0.5").to(sl.childNodes[3].childNodes[3], 0.3, { top: "18%", left: "26%", width: "100%", ease: Power4.easeOut });
+            }
+
+            // tl.to(sl, 0, {autoAlpha:1}, '-=0.3')
+            //     .to(sl.childNodes[3].childNodes[3], 0.1, {autoAlpha:1}, "-=0.5")
+            //         .to(sl.childNodes[3].childNodes[3], 0.3, {top: "47%",left: "-11%",width: "70%", ease: Power4.easeOut, delay: 0.5});
+
+            sl.classList.remove('is-hidden');
+
+            // console.log('+====',  sl.childNodes[3].childNodes[3])
+        }
+    });
+
+    // console.log('-data.from-', typeof data.from, typeof data.to)
+
+    //Show current dot
+    paginationBtn.forEach(function (btnSl) {
+
+        if (btnSl.dataset.gotoslide == data.from) btnSl.classList.remove('is-active');
+        if (btnSl.dataset.gotoslide == data.to) btnSl.classList.add('is-active');
+    });
+});
+PubSub.subscribe('gotoSlide.scroll', function (msg, data) {
+
     // function setActiveEl(isEl, isAtrr, isClass){
-    //
-    //     console.log(':|',isEl, ':|');
-    //
+
     //     isEl.forEach( (sl,el) => {
     //
     //         if ( sl.dataset.isAtrr == data.from ) sl.classList.remove(isClass);
@@ -91,25 +178,19 @@ PubSub.subscribe('gotoSlide', function (msg, data) {
     //     });
     //
     // }
-    //
+
     // setActiveEl(slideAll, slide,'is-active');
     // setActiveEl(paginationBtn, gotoslide 'is-active');
 
-    slideAll.forEach(function (sl) {
+});
+PubSub.subscribe('gotoSlide.top', function (msg, data) {
 
-        if (sl.dataset.slide == data.from) tl.fromTo(sl, .25, { opacity: 1 }, { opacity: 0 });
-        if (sl.dataset.slide == data.to) tl.fromTo(sl, .25, { opacity: 0 }, { opacity: 1 });
+    // //Show current dot
+    // paginationBtn.forEach( btnSl => {
+    //
+    //     if ( btnSl.dataset.gotoslide == data.from ) btnSl.classList.remove('is-active');
+    //     if ( btnSl.dataset.gotoslide == data.to ) btnSl.classList.add('is-active');
+    //
+    // });
 
-        // tl.fromTo(sl.dataset.item == 1 , .3, {opacity:0, width: 30}, {opacity:1, width: 40});
-
-
-        var getImg = document.getElementsByTagName('img');
-        console.log(getImg);
-    });
-
-    paginationBtn.forEach(function (btnSl) {
-
-        if (btnSl.dataset.gotoslide == data.from) btnSl.classList.remove('is-active');
-        if (btnSl.dataset.gotoslide == data.to) btnSl.classList.add('is-active');
-    });
 });
--- src/js/plugin.js
+++ src/js/plugin.js
@@ -1,7 +1,11 @@
-let tl              = TweenLite;
+let twl             = TweenLite;
+let tl              = new TimelineLite();
 let slider          = document.getElementById('slider-gaps');
 let paginationBtn   = document.querySelectorAll('#pagination li a');
 let slideAll        = slider.querySelectorAll('.slide');
+
+tl.staggerFrom(document.querySelectorAll('.btn-dot'), 0.15, {x: 0,y:-100, autoAlpha: 0,ease: Power4.easeOut, delay: 1}, 0.1);
+
 
 class SliderGsap{
 
@@ -12,29 +16,35 @@ class SliderGsap{
         this.activeSlide    = 1;
         this.canGo          = true;
         this.maxSlide       = 5;
+        this.startSlide = 0;
 
     }
+
+
 
     scrollEvent(){
 
         slider.addEventListener('wheel', e => {
 
+            // console.log(e);
+
             if( !this.canGo ) return;
             this.canGo = false;
 
             let direction = e.wheelDeltaY < 0 ? 1 : -1 ;
-            let newSlide = +this.activeSlide + (+direction);
+            let newSlide = this.activeSlide + (direction);
 
             if( newSlide > this.maxSlide ) newSlide = 1;
             if( newSlide < 1 ) newSlide = 5;
 
-            PubSub.publish('gotoSlide', { from: this.activeSlide, to: +newSlide });
+            PubSub.publish('gotoSlide', { from: this.activeSlide, to: newSlide});
+            PubSub.publish('gotoSlide.scroll', { start: true });
 
             this.activeSlide = newSlide;
 
             setTimeout( () => {
                 this.canGo = true;
-            }, 600)
+            }, 1300)
 
         }, {passive: true});
 
@@ -54,7 +64,8 @@ class SliderGsap{
 
                 if( newSlide !== this.activeSlide ){
 
-                    PubSub.publish('gotoSlide', { from: +this.activeSlide, to: +newSlide });
+                    PubSub.publish('gotoSlide', { from: this.activeSlide, to: newSlide });
+                    PubSub.publish('gotoSlide.top', { start: true , from: this.activeSlide, to: newSlide});
 
                     this.activeSlide = newSlide;
 
@@ -66,15 +77,123 @@ class SliderGsap{
     }
 
 }
+
 let slide = new SliderGsap();
+
 
 PubSub.subscribe('gotoSlide', function (msg, data) {
 
+    // console.log( msg, data );
+
+
+    //Show current slide
+    slideAll.forEach( (sl,d,i) => {
+
+        // console.log('-= sl =-',d, sl,'i|::',i);
+
+        if ( sl.dataset.slide == data.from ) {
+
+            sl.classList.remove('is-active');
+            sl.dataset.state = ('hidden');
+
+            if ( data.from == 1 ) {
+                console.log('| data.from == 1 |', sl.childNodes);
+                tl.to(sl.childNodes[3].childNodes[1], 0.3, {  autoAlpha:0 })
+                    .to(sl.childNodes[3].childNodes[3], 0.3, {  autoAlpha:1 }, '-=0.3')
+                        .to(sl, 0.25, {autoAlpha:0, delay: 0.25 });
+
+            }
+            if ( data.from == 2 ) {
+                console.log('| data.from == 2 |');
+                tl.to(sl.childNodes[3].childNodes[3], 0.3, { top: "16.2%",left: "23.8%",width: "42%", ease: Power4.easeOut})
+                    .to(sl.childNodes[3].childNodes[3], 0.1, { autoAlpha:0, delay: 0.25 })
+                        .to(sl, 0.25, {autoAlpha:0, ease: Power4.easeOut }, '-=0.3');
+                
+            }
+            if ( data.from == 3 ) {
+                console.log('| data.from == 3 |');
+
+                tl.to(sl.childNodes[3].childNodes[3], 0.3, {top: "44%",left: "32%",width: "41%", ease: Power4.easeOut })
+                    .to(sl.childNodes[3].childNodes[3], 0.1, {autoAlpha:0, delay: 0.25 })
+                        .to(sl, 0, {autoAlpha:0 }, '-=0.3');
+
+            }
+
+            console.log('*** sl.dataset.slide ***', sl.dataset.slide);
+
+            // tl.to(sl.childNodes[3].childNodes[3], 0.3, { top: "14.2%",left: "23.8%",width: "42%", delay: 0.5 })
+            //     .to(sl.childNodes[3].childNodes[3], 0.1, { autoAlpha:0})
+            //            .to(sl, 0.25, {autoAlpha:0, ease: Power4.easeOut}, '-=0.3');
+
+
+            sl.classList.add('is-hidden');
+
+
+
+            // console.log('-====',  sl.childNodes[3].childNodes[3])
+
+
+        }
+        if ( sl.dataset.slide == data.to ) {
+
+            sl.classList.add('is-active');
+            sl.dataset.state = ('visibility');
+
+            if ( data.to == 1 ) {
+                console.log('| data.from == 1 |', sl.childNodes);
+
+                tl.to(sl, 0.25, {autoAlpha:1, ease: Power4.easeOut }, '-=0.3')
+                    .to(sl.childNodes[3].childNodes[3], 0.3, {  autoAlpha:0 })
+                        .to(sl.childNodes[3].childNodes[1], 0.3, {  autoAlpha:1 }, '-=0.3');
+
+            }
+
+            if ( data.to == 2 ) {
+
+                tl.to(sl, 0, {autoAlpha:1, ease: Power4.easeOut }, '-=0.3')
+                    .to(sl.childNodes[3].childNodes[3], 0.1, {autoAlpha:1}, "-=0.5")
+                        .to(sl.childNodes[3].childNodes[3], 0.3, {top: "47%",left: "-11%",width: "70%", ease: Power4.easeOut});
+
+            }
+
+            if ( data.to == 3 ) {
+
+                tl.to(sl, 0, {autoAlpha:1, ease: Power4.easeOut }, '-=0.3')
+                    .to(sl.childNodes[3].childNodes[3], 0.1, {autoAlpha:1}, "-=0.5")
+                        .to(sl.childNodes[3].childNodes[3], 0.3, {top: "18%",left: "26%",width: "100%", ease: Power4.easeOut });
+
+            }
+
+
+            // tl.to(sl, 0, {autoAlpha:1}, '-=0.3')
+            //     .to(sl.childNodes[3].childNodes[3], 0.1, {autoAlpha:1}, "-=0.5")
+            //         .to(sl.childNodes[3].childNodes[3], 0.3, {top: "47%",left: "-11%",width: "70%", ease: Power4.easeOut, delay: 0.5});
+
+            sl.classList.remove('is-hidden');
+
+
+            // console.log('+====',  sl.childNodes[3].childNodes[3])
+
+        }
+
+    });
+
+    // console.log('-data.from-', typeof data.from, typeof data.to)
+
+    //Show current dot
+    paginationBtn.forEach( btnSl => {
+
+        if ( btnSl.dataset.gotoslide == data.from ) btnSl.classList.remove('is-active');
+        if ( btnSl.dataset.gotoslide == data.to ) btnSl.classList.add('is-active');
+
+    });
+
+
+});
+PubSub.subscribe('gotoSlide.scroll', function (msg, data){
 
     // function setActiveEl(isEl, isAtrr, isClass){
-    //
-    //     console.log(':|',isEl, ':|');
-    //
+
     //     isEl.forEach( (sl,el) => {
     //
     //         if ( sl.dataset.isAtrr == data.from ) sl.classList.remove(isClass);
@@ -84,30 +203,20 @@ PubSub.subscribe('gotoSlide', function (msg, data) {
     //     });
     //
     // }
-    //
+
     // setActiveEl(slideAll, slide,'is-active');
     // setActiveEl(paginationBtn, gotoslide 'is-active');
 
-    slideAll.forEach( sl => {
+});
+PubSub.subscribe('gotoSlide.top', function (msg, data){
 
-        if ( sl.dataset.slide == data.from ) tl.fromTo(sl, .25, {opacity:1}, {opacity:0});
-        if ( sl.dataset.slide == data.to ) tl.fromTo(sl, .25, {opacity:0}, {opacity:1});
-
-        // tl.fromTo(sl.dataset.item == 1 , .3, {opacity:0, width: 30}, {opacity:1, width: 40});
-
-
-        let getImg = document.getElementsByTagName('img');
-        console.log(getImg);
-
-
-    });
-
-    paginationBtn.forEach( btnSl => {
-
-        if ( btnSl.dataset.gotoslide == data.from ) btnSl.classList.remove('is-active');
-        if ( btnSl.dataset.gotoslide == data.to ) btnSl.classList.add('is-active');
-
-    });
+    // //Show current dot
+    // paginationBtn.forEach( btnSl => {
+    //
+    //     if ( btnSl.dataset.gotoslide == data.from ) btnSl.classList.remove('is-active');
+    //     if ( btnSl.dataset.gotoslide == data.to ) btnSl.classList.add('is-active');
+    //
+    // });
 
 });
 
